02b48e3dd2fb9e3dbf399ef7b254c179d4b77d4e
jani-nykanen <jnyknn@gmail.com>
2018-02-22 21:54:56 +0200

    Ending files

---
+++ src/ending.c
@@ -0,0 +1,4 @@
+/// Ending (source)
+/// (c) 2018 Jani Nykänen
+
+#include "ending.h"
---
+++ src/ending.h
@@ -0,0 +1,8 @@
+/// Ending (header)
+/// (c) 2018 Jani Nykänen
+
+#ifndef __ENDING__
+#define __ENDING__
+
+
+#endif // __ENDING__
